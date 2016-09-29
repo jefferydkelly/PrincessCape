233f89b5cad5888071d5ed4d542d700fca6ed6fe
JD Kelly <jd9162@rit.edu>
2016-09-29 10:55:07 -0400

    Imported Sprites. Started working on code.

---
+++ Princess of Thieves/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager {
+	private static GameManager instance = null;
+
+	private GameManager()
+	{
+	}
+	public static GameManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = new GameManager();
+			}
+
+			return instance;
+		}
+	}
+}
+
+public enum GameState {
+	None,
+	Play,
+	Menu,
+	Cutscene,
+	Pause
+}
