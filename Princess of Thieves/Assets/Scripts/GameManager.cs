1191dfec9278e1752967b7a4a09cb724fbbb033e
JD Kelly <jd9162@rit.edu>
2016-09-29 12:57:01 -0400

    Started reworking controller to be only one class.

--- Princess of Thieves/Assets/Scripts/GameManager.cs
+++ Princess of Thieves/Assets/Scripts/GameManager.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class GameManager {
 	private static GameManager instance = null;
+	private GameState state;
 
 	private GameManager()
 	{
+		state = GameState.Play;
+		//Set up the UI Manager
+		//Set up the controller info and pass that to the player
 	}
 	public static GameManager Instance
 	{
@@ -19,12 +23,23 @@ public class GameManager {
 			return instance;
 		}
 	}
+
+	public bool IsPaused
+	{
+		get
+		{
+			return (state & GameState.Paused) > 0;
+		}
+	}
 }
 
+[System.Flags]
 public enum GameState {
-	None,
-	Play,
-	Menu,
-	Cutscene,
-	Pause
+	None = 0,
+	Play = 1,
+	Menu = 2,
+	Cutscene = 4,
+	GameOver = 8,
+	Paused = 16,
+	Any = int.MaxValue
 }
