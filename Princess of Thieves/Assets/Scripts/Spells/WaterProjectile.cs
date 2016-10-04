e8f3159cb2a08e7f15b1efcb6bc27755804426a8
JD Kelly <jd9162@rit.edu>
2016-10-04 13:58:26 -0400

    Waves now pass through a set number of things before being destroyed.

--- Princess of Thieves/Assets/Scripts/Spells/WaterProjectile.cs
+++ Princess of Thieves/Assets/Scripts/Spells/WaterProjectile.cs
@@ -5,32 +5,46 @@ public class WaterProjectile : SpellProjectile {
 
 	float startForce = 10;
 	float lifeTime = 5;
+	float oldXVel = 0;
+	int numberOfThingsToPassThrough = 5;
+	int passedThrough = 0;
+	Rigidbody2D myRigidbody;
 	// Use this for initialization
 	void Start () {
 		SpriteRenderer sr = gameObject.AddComponent<SpriteRenderer>();
 		sr.sprite = Resources.Load<Sprite>("Sprites/Wave");
 		gameObject.AddComponent<BoxCollider2D>();
-		Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
-		rb.AddForce(fwd * startForce, ForceMode2D.Impulse);
-		rb.freezeRotation = true;
-		rb.mass = 0.5f;
+		myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.AddForce(fwd * startForce, ForceMode2D.Impulse);
+		myRigidbody.freezeRotation = true;
+		myRigidbody.mass = 0.5f;
 		Invoke("Destroy", lifeTime);
 	}
 
+	void FixedUpdate()
+	{
+		oldXVel = myRigidbody.velocity.x;
+	}
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if (!col.gameObject.CompareTag("Platform"))
 		{
-			Debug.Log(col.gameObject.tag);
 			DamageableObject dObj = col.gameObject.GetComponent<DamageableObject>();
 
 			if (dObj != null && dObj.Allegiance != allegiance)
 			{
 				dObj.TakeDamage(new DamageSource(DamageType.Water, damage, allegiance));
-
 			}
 
-			Destroy();
+			Physics2D.IgnoreCollision(GetComponent<Collider2D>(), col.collider);
+			myRigidbody.velocity = new Vector2(oldXVel, myRigidbody.velocity.y);
+
+			passedThrough++;
+
+			if (passedThrough >= numberOfThingsToPassThrough)
+			{
+				Destroy();
+			}
 		}
 	}
 
