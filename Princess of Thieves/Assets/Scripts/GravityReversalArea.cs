8e22300a9d9684794c665230af9ff0808fecf60f
JD Kelly <jd9162@rit.edu>
2016-10-20 10:46:28 -0400

    Created the GravityReversalArea prefab and script which reverses the gravity scale of any Rigidbody2D that enters it. Reworked how the player handles jumping/being on the ground to work with reversing gravity.

---
+++ Princess of Thieves/Assets/Scripts/GravityReversalArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityReversalArea : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		
+		Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+
+		if (rb)
+		{
+			rb.gravityScale *= -1;
+			Debug.Log("New Gravity: " + rb.gravityScale);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		
+		Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+
+		if (rb)
+		{
+			rb.gravityScale *= -1;
+			Debug.Log("Resetting polarity back to: " + rb.gravityScale);
+		}
+	}
+}
