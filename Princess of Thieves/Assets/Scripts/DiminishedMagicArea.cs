e6ae335d3e7b329bee975b67393d478205a4eb39
JD Kelly <jd9162@rit.edu>
2016-10-20 11:35:28 -0400

    Created the prefab and the script for the DiminishedMagicArea which diminishes the effects of projectiles that enter it.

---
+++ Princess of Thieves/Assets/Scripts/DiminishedMagicArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiminishedMagicArea : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		SpellProjectile sp = col.GetComponent<SpellProjectile>();
+
+		if (sp)
+		{
+			sp.Diminish();
+		}
+	}
+}
