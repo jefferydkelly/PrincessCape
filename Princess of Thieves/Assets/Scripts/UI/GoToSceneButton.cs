30b407da3642732901eb8dfeb882782deac1b65f
JD Kelly <jd9162@rit.edu>
2016-11-03 13:00:04 -0400

    Created a TitleMenu and started working on custom UI Elements that will work with the ControllerClass.

---
+++ Princess of Thieves/Assets/Scripts/UI/GoToSceneButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToSceneButton : UIElement {
+	public string scene;
+	public override void Click()
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
