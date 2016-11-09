0bd7c901f71f4d633f508c5cc6c35645f9dc3528
Rose <cinorflynn@yahoo.com>
2016-11-09 12:19:21 -0500

    Eye and hand prefab, with scripts. There's also a mordil manager script, which is where the boss fight conditions will take place. The eyes move and try to track the player, but the hands don't have any actions yet.

---
+++ Princess of Thieves/Assets/Scripts/MordilManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MordilManager : MonoBehaviour {
+    static MordilManager instance = null;
+    List<BossEyeScript> mordEyes;
+    List<HandOfMordil> mordHands;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public static MordilManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new MordilManager();
+            }
+
+            return instance;
+        }
+    }
+}
