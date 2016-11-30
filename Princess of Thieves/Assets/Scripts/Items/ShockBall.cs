781328cc0a71f991d39630a282daa66d468aefd1
JD Kelly <jdk916@rit.edu>
2016-11-30 11:53:01 -0500

    Created the Shock Shooter and the Shock Ball.  Shock Shooter fires Shock Balls.  Shock balls collide with non-background, non-player objects and in a future update, will stun enemies in place for a short time.

---
+++ Princess of Thieves/Assets/Scripts/Items/ShockBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockBall : MonoBehaviour {
+
+    Vector3 fwd;
+    float speed = 20;
+	// Use this for initialization
+	void Start () {
+        Player p = GameManager.Instance.Player;
+        transform.position = p.transform.position;
+        fwd = p.Forward;
+	}
+
+    void Update()
+    {
+        transform.position += fwd * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TagContains("Enemy"))
+        {
+            //Get the enemy and shock them
+            Debug.Log("Hit an enemy");
+            Destroy(gameObject);
+        } else if (!collision.OnLayer("Background") && !collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+}
