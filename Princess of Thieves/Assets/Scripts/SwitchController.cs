51e95624daae202de9184252562a1e7f1810ad8b
JD Kelly <jdk916@rit.edu>
2016-12-13 11:41:33 -0500

    Created sprites and a gameobject  for new seed activated switches.

---
+++ Princess of Thieves/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : ActivatorObject {
+    private bool activated;
+    public Sprite activatedSprite;
+    public Sprite deactivatedSprite;
+    private SpriteRenderer myRenderer;
+    public void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Seed"))
+        {
+            if (activated)
+            {
+                Deactivate();
+            } else
+            {
+                Activate();
+            }
+        }
+    }
+
+    private bool Activated
+    {
+        get
+        {
+            return activated;
+        }
+
+        set
+        {
+            activated = value;
+            myRenderer.sprite = activated ? activatedSprite : deactivatedSprite;
+        }
+    }
+}
