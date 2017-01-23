cfaf5244e12e325a86d3db230be4af97cc49873f
JD Kelly <jdk9162@rit.edu>
2017-01-23 14:14:03 -0500

    Added in a new Sprite showing a dead knight to serve as the new treasure chest.

---
+++ Princess of Thieves/Assets/Scripts/KinghtCorpse.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinghtCorpse : JDMappableObject, InteractiveObject {
+
+	public Sprite openedSprite;
+	public Sprite closedSprite;
+	public Sprite highlightedSprite;
+	bool looted = false;
+	[SerializeField]
+	GameObject contents;
+	SpriteRenderer myRenderer;
+
+	private void Start()
+	{
+		myRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void Interact()
+	{
+		if (!looted)
+		{
+			UIManager.Instance.ShowInteraction("");
+			GameManager.Instance.Player.AddItem(contents);
+			//myRenderer.sprite = openedSprite;
+			looted = true;
+		}
+	}
+
+	public void Highlight()
+	{
+		if (!looted)
+		{
+			UIManager.Instance.ShowInteraction("Loot");
+			//myRenderer.sprite = highlightedSprite;
+		}
+	}
+
+	public void Dehighlight()
+	{
+		if (!looted)
+		{
+			UIManager.Instance.ShowInteraction("");
+			//myRenderer.sprite = closedSprite;
+		}
+	}
+}
