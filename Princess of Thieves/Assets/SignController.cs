182059f3e3ffa6fbc0dd58e839fae635b1f2aeae
JD Kelly <jdk9162@rit.edu>
2017-01-25 15:57:38 -0500

    Updated CapeTestScene to better serve as a tutorial and show off how the cape might be used. Created a new Sign prefab and a script to allow for signs to be read to give the player little pieces of (generally humorous) information.

---
+++ Princess of Thieves/Assets/SignController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour, InteractiveObject {
+	[SerializeField]
+	string[] message;
+	SpriteRenderer myRenderer;
+	public void Start() {
+		myRenderer = GetComponent<SpriteRenderer> ();
+	}
+	public void Interact() {
+		StartCoroutine(UIManager.Instance.ShowFoundItemMessage (message));
+	}
+
+	public void Highlight() {
+		UIManager.Instance.ShowInteraction ("Read");
+		myRenderer.material.color = Color.blue;
+	}
+
+	public void Dehighlight() {
+		UIManager.Instance.ShowInteraction ("");
+		myRenderer.material.color = Color.white;
+	}
+}
