6500b54ccab4a21d0b865e5cd9fb00c260ab8a7a
JD Kelly <jdk9162@rit.edu>
2017-01-26 12:02:01 -0500

    Moved SignController to Scripts. Created a new Fan prefab and script to push things in certain directions.  It can be turned on and off like magnets but it affects everything.

---
+++ Princess of Thieves/Assets/Scripts/FanController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour, ActivateableObject {
+
+	[SerializeField]
+	Vector2 fwd = new Vector2(1, 0);
+	float force = 10;
+	float range = 10;
+	[SerializeField]
+	bool isActive = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsActive) {
+			foreach (RaycastHit2D hit in Physics2D.RaycastAll(transform.position, fwd, range)) {
+				Rigidbody2D rb = hit.collider.GetComponent<Rigidbody2D> ();
+
+				if (rb) {
+					rb.AddForce (fwd * force);
+				}
+			}
+		}
+	}
+
+	public void Activate() {
+		isActive = true;
+	}
+
+	public void Deactivate() {
+		isActive = false;
+	}
+
+	public bool IsActive {
+		get {
+			return isActive;
+		}
+	}
+}
