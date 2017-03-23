ab280cfd643ef5b9821216c0bb1cc04269352188
Rose <cinorflynn@yahoo.com>
2017-03-23 13:57:29 -0400

    Added a few new scripts for working within the ending cutscene. Nothing broke with the other cutscenes (I tested them).

---
+++ Princess of Thieves/Assets/DragonsHeadScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonsHeadScript : MonoBehaviour {
+    public float timeTilActivate = 3f;
+	// Use this for initialization
+	void Awake () {
+        Timer t = new Timer(() => {
+            ActivateCutscene(); 
+        }, timeTilActivate);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ActivateCutscene()
+    {
+        Debug.Log("Hello");
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "TopCeiling") //jank, but works
+        {
+            Debug.Log("Hello");
+
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+}
