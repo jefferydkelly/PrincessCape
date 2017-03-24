47cd24b0d79049f964a5bd5c5a16d3bb8c554f6b
Rose <cinorflynn@yahoo.com>
2017-03-24 14:24:20 -0400

    Added the staying portion of the choice.

--- Princess of Thieves/Assets/DragonsHeadScript.cs
+++ Princess of Thieves/Assets/DragonsHeadScript.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragonsHeadScript : MonoBehaviour {
+    public TextAsset cutscene;
     public float timeTilActivate = 3f;
+    bool timed = false;
+    public Timer t;
 	// Use this for initialization
 	void Awake () {
-        Timer t = new Timer(() => {
-            ActivateCutscene(); 
+        t = new Timer(() => {
+            timed = true;
         }, timeTilActivate);
+        t.Start();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (timed)
+        {
+            ActivateCutscene();
+            timed = false;
+        }
 	}
 
     void ActivateCutscene()
     {
-        Debug.Log("Hello");
+        //incoming bad code lmao
+        GameObject temp = Instantiate(gameObject, transform.position, transform.rotation);
+        temp.AddComponent<CutsceneTrigger>();
+        temp.GetComponent<CutsceneTrigger>().cutscene = cutscene;
+        temp.GetComponent<SpriteRenderer>().enabled = false;
+        temp.GetComponent<DragonsHeadScript>().t.Stop();
+
+        //wait some time, then load an ending scene.
     }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "TopCeiling") //jank, but works
         {
-            Debug.Log("Hello");
-
+            t.Stop();
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
