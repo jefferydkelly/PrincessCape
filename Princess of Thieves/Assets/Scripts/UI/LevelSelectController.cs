1136c49a2a3f68b5f27717b18fd0b6b9cc1df028
JD Kelly <jdk9162@rit.edu>
2017-03-27 13:59:05 -0400

    Players can now cycle through levels using the Up and Down arrow buttons.

--- Princess of Thieves/Assets/Scripts/UI/LevelSelectController.cs
+++ Princess of Thieves/Assets/Scripts/UI/LevelSelectController.cs
@@ -8,6 +8,7 @@ public class LevelSelectController : MenuController{
 	string[] levels;
 	string [] levelNames;
 	int numButtons = 4;
+	int startIndex = 0;
 	// Use this for initialization
 	void Start () {
 		Setup ();
@@ -29,5 +30,32 @@ public class LevelSelectController : MenuController{
 				ChangeScene(level);
 			});
 		}
+	}
+
+	void UpdateButtons() {
+		for (int i = 0; i < numButtons; i++) {
+			Button button = buttons [i];
+			int levInd = (startIndex + i) % levels.Length;
+			string level = levels [levInd];
+			button.GetComponentInChildren<Text> ().text = levelNames[levInd];
+			button.onClick.RemoveAllListeners ();
+			button.onClick.AddListener (delegate() {
+				ChangeScene(level);
+			});
+		}
+	}
+
+	public void IncIndex() {
+		startIndex = (startIndex + 1) % levelNames.Length;
+		UpdateButtons ();
+	}
+
+	public void DecIndex() {
+		startIndex -= 1;
+
+		while (startIndex < 0) {
+			startIndex += levels.Length;
+		}
+		UpdateButtons ();
 	}
 }
