d74860c675cf3204b9887b4301b2e1a18387bae5
Rose <cinorflynn@yahoo.com>
2017-04-08 12:48:32 -0400

    Precursor to enemy beams for magnetic wave momentum

---
+++ Princess of Thieves/Assets/tempWaveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempWaveScript : MonoBehaviour
+{
+    public GameObject target;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //0.2
+    
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Contains("Metal"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
